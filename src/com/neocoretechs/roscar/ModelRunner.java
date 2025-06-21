a9967c56dcdcabb1e74f34d70024bf0015348bc0
NeoCoreTechs - New Core Technologies <groffj@neocoretechs.com>
2025-06-21 13:19:43 -0700

    Integrated RelatrixLSH; store/query FloatVector to Realtrix DB with LSH index

--- src/com/neocoretechs/roscar/ModelRunner.java
+++ src/com/neocoretechs/roscar/ModelRunner.java
@@ -67,9 +67,11 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import com.neocoretechs.relatrix.client.asynch.AsynchRelatrixClientTransaction;
+import com.neocoretechs.relatrix.Relatrix;
 import com.neocoretechs.relatrix.Result;
 import com.neocoretechs.rocksack.TransactionId;
 import com.neocoretechs.rocksack.Alias;
+import com.neocoretechs.relatrix.DuplicateKeyException;
 
 public class ModelRunner {
     // Batch-size used in prompt evaluation.
@@ -3731,4 +3733,194 @@ final class HashTable implements Serializable {
 		return String.format("%s index=%d family=%s hashes=%s tableSize=%d",this.getClass().getName(), index, family, Arrays.toString(hashFunctions), hashTable.size());
 	}
 }
+/**
+ * An {@link Index} contains one or more locality sensitive hash tables. These hash
+ * tables contain the mapping between a combination of a number of hashes
+ * (encoded using an integer) and a list of possible nearest neighbors.<p>
+ *
+ * A hash function can hash a vector of arbitrary dimensions to an integer
+ * representation. The hash function needs to be locality sensitive to work in
+ * the locality sensitive hash scheme. Meaning that vectors that are 'close'
+ * according to some metric have a high probability to end up with the same
+ * hash.<p>
+ * In the context of Locality-Sensitive Hashing (LSH), w represents the bucket width or window size.<p>
+ * When we compute the hash value for a vector using a random projection. Here's what each component does:<p>
+ * vector.dot(randomProjection): Computes the dot product of the input vector and a random projection vector. <br>
+ * This projects the input vector onto a random direction.<br>
+ * offset: Adds a random offset to the projected value. <br>
+ * This helps to shift the projected values and create a more uniform distribution.<p>
+ * w: The bucket width or window size. This value determines the granularity of the hash function.<br>
+ * By dividing the projected value (plus offset) by w, you're essentially:<br>
+ * Quantizing the projected values into discrete buckets.<br>
+ * Assigning each bucket a unique hash value. <br>
+ * The choice of w affects the trade-off between:<br>
+ * Precision: Smaller w values result in more precise hashing, but may lead to more collisions.
+ * Larger w values result in fewer collisions, but may reduce precision.<p>
+ * In general, w is a hyperparameter that needs to be tuned for specific applications and datasets. 
+ * A good choice of w can significantly impact the performance of the LSH algorithm.<p>
+ * This class is designed to be stored in the Relatrix database to serve as a template for encoding and retrieving
+ * a given set of floating point tensors.
+ * @author Jonathan Groff Copyright (C) NeoCoreTechs 2025
+ */
+final class RelatrixLSH implements Serializable, Comparable {
+	private static final long serialVersionUID = -5410017645908038641L;
+	private static boolean DEBUG = true;
+	public static final int VECTOR_DIMENSION = 50;
+	public static int numberOfHashTables = 16;
+	public static int numberOfHashes = 12;
+	public static Relatrix relatrix;
+
+	/**
+	 * Contains the mapping between a combination of a number of hashes (encoded
+	 * using an integer) and a list of possible nearest neighbours
+	 */
+	private List<CosineHash[]> hashTable;
+	private UUID key;
+	
+	public RelatrixLSH() {}
+	/**
+	 * Initialize a new hash table, it needs a hash family and a number of hash
+	 * functions that should be used.
+	 * 
+	 * @param numberOfHashes
+	 *            The number of hash functions that should be used.
+	 * @param family
+	 *            The hash function family knows how to create new hash
+	 *            functions, and is used therefore.
+	 */
+	public RelatrixLSH(int numberOfHashes, int numberOfHashTables, int projectionVectorSize) {
+		this.key = UUID.randomUUID();
+		this.hashTable = new ArrayList<CosineHash[]>();
+		for(int i = 0; i < numberOfHashTables; i++) {
+			final CosineHash[] cHash = new CosineHash[numberOfHashes];
+			this.hashTable.add(cHash);
+			if(numberOfHashes > 64)
+				Parallel.parallelFor(0, numberOfHashes, j -> {
+					cHash[j] = new CosineHash(projectionVectorSize);
+				});
+			else
+				for(int j = 0; j < numberOfHashes; j++)
+					cHash[j] = new CosineHash(projectionVectorSize);
+		}
+	}
+	
+	public UUID getKey() {
+		return key;
+	}
+	
+	/**
+	 * Query the hash table for a vector. It calculates the hash for the vector,
+	 * and does a lookup in the hash table. If no candidates are found, an empty
+	 * list is returned, otherwise, the list of candidates is returned.
+	 * 
+	 * @param query
+	 *            The query vector.
+	 * @return Does a lookup in the table for a query using its hash. If no
+	 *         candidates are found, an empty list is returned, otherwise, the
+	 *         list of candidates is returned as word, List<FloatTensor>
+	 * @throws IOException 
+	 * @throws IllegalAccessException 
+	 * @throws ClassNotFoundException 
+	 * @throws IllegalArgumentException 
+	 */
+	public List<Result> query(FloatTensor query) throws IllegalArgumentException, ClassNotFoundException, IllegalAccessException, IOException {
+		ArrayList<Result> res = new ArrayList<Result>();
+		for(int i = 0; i < hashTable.size(); i++) {
+			Integer combinedHash = hash(hashTable.get(i), query);
+			if(DEBUG)
+				System.out.println("Querying combined hash for query "+i+" of "+hashTable.size()+":"+combinedHash);
+			Iterator<?> it = relatrix.findSet(combinedHash, '?', '?');
+			int cnt = 0;
+			while(it.hasNext()) {
+				res.add((Result) it.next());
+				System.out.print(++cnt+"\r");
+			}
+			System.out.println();
+		}
+		return res;
+	}
+	public List<Result> queryParallel(FloatTensor query) throws IllegalArgumentException, ClassNotFoundException, IllegalAccessException, IOException {
+		List<Result> res = new ArrayList<Result>();
+		ArrayList<Object> iq = new ArrayList<Object>();
+		for(int i = 0; i < hashTable.size(); i++) {
+			Integer combinedHash = hash(hashTable.get(i), query);
+			iq.add(combinedHash);
+		}
+		long tims = System.currentTimeMillis();
+		if(DEBUG)
+			System.out.println("Querying combined hash for table of "+hashTable.size());
+		res = relatrix.findSetParallel(iq, '?', '?');
+		if(DEBUG)
+			System.out.println((System.currentTimeMillis()-tims)+" ms.");
+		return res;
+	}
+
+	/**
+	 * Add a vector to the index.
+	 * @param word the word that vectorized
+	 * @param vector the embedding of the word
+	 * @throws DuplicateKeyException 
+	 * @throws IOException 
+	 * @throws ClassNotFoundException 
+	 * @throws IllegalAccessException 
+	 */
+	public void add(String word, FloatTensor vector) throws IllegalAccessException, ClassNotFoundException, IOException {
+		for(int i = 0; i < hashTable.size(); i++) {
+			Integer combinedHash = hash(hashTable.get(i), vector);
+			try {
+				relatrix.store(combinedHash, word, vector);
+			} catch (DuplicateKeyException e) {
+				System.out.println("duplicate key:"+combinedHash+" for "+word);
+			}
+		}
+	}
+	
+	/**
+	 * Calculate the combined hash for a vector.
+	 * @param hash one of numberOfHashes
+	 * @param vector The vector to calculate the combined hash for.
+	 * @return An integer representing a combined hash.
+	 */
+	private Integer hash(CosineHash[] hash, FloatTensor vector){
+		int hashes[] = new int[hash.length];
+		for(int i = 0 ; i < hash.length ; i++){
+			hashes[i] = hash[i].hash(vector);
+		}
+		Integer combinedHash = CosineHash.combine(hashes);
+		return combinedHash;
+	}
+
+	/**
+	 * Return the number of hash functions used in the hash table.
+	 * @return The number of hash functions used in the hash table.
+	 */
+	public int getNumberOfHashes() {
+		return hashTable.get(0).length;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s key=%s tables=%d hashes=%d",this.getClass().getName(), key, numberOfHashTables, numberOfHashes);
+	}
+	
+	@Override
+	public int compareTo(Object o) {
+		int key0 = key.compareTo(((RelatrixLSH)o).key);
+		if(key0 != 0)
+			return key0;
+		for(int i = 0; i < hashTable.size(); i++) {
+			CosineHash[] cos0 = hashTable.get(i);
+			CosineHash[] cos1 = (((RelatrixLSH)o).hashTable.get(i));
+			for(int j = 0; j < cos0.length; j++) {
+				if(j >= cos1.length)
+					return 1;
+				int key1 = cos0[j].compareTo(cos1[j]);
+				if(key1 != 0)
+					return key1;
+			}
+		}
+		return 0;
+	}
+}
+
 
